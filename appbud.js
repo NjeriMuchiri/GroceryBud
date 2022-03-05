4b0dfb52ab606f64509c4fca63d789ec2d761a70
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-03-05 10:16:13 +0300

    working with local storage

--- appbud.js
+++ appbud.js
@@ -87,7 +87,7 @@ function clearItems(){
     container.classList.remove("show-container");
     displayAlert('empty list', 'danger');
     setBackToDefault();
-    // localStorage.removeItem('list)
+    localStorage.removeItem('list');
 }
 //   delete function
    function deleteItem(e){
@@ -100,7 +100,7 @@ function clearItems(){
        displayAlert('item removed','danger');
        setBackToDefault();
     //    remove from local storage
-    // removeFromLocalStorage(id);
+    removeFromLocalStorage(id);
    }
 // edit function
 function editItem(e){
@@ -123,26 +123,38 @@ function editItem(e){
 // local storage
 function addToLocalStorage(id,value){
    const grocery = {id,value};
-   let items = localStorage.getItem("list")
-    ? JSON.parse(localStorage.getItem('list'))
-    : [];
-    console.log(items);
+   let items = getLocalStorage();
 
    items.push(grocery);
    localStorage.setItem('list',JSON.stringify(items));
 }
+function getLocalStorage() {
+  return localStorage.getItem("list")
+    ? JSON.parse(localStorage.getItem("list"))
+    : [];
+}
+
 function removeFromLocalStorage(id){
      let items = getLocalStorage();
 
      items = items.filter(function(item){
-         if(item.id !==id ){
+         if(item.id !== id ){
              return item;
          }
-     })
+     });
    localStorage.setItem('list',JSON.stringify(items));
 
 }
 function editLocalStorage(id, value){
+     let items = getLocalStorage();
+
+  items = items.map(function (item) {
+    if (item.id === id) {
+      item.value = value;
+    }
+    return item;
+  });
+  localStorage.setItem("list", JSON.stringify(items));
 
 }
 // localStorage API
